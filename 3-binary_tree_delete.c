1464f90c345ce75f7523706c5281fd328c027860
Harold-FN-2187 <laryeaharold319@gmail.com>
2024-03-29 15:08:45 +0000

    x5.00002

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,9 +1,9 @@
-#include "binary_trees.h"'
-
+#include "binary_trees.h"
 /**
- *binary_tree_delete - delete an entire binary tree
- *@tree: tree to delete
+ * binary_tree_delete - deletes an entire binary tree
+ * @tree: tree to be deleted
 */
+
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (!tree)
